2831a1e01d4001ff3db29cae818ee2563c51fa48
Mingyueyixi <1379598589@qq.com>
2023-01-24 23:30:42 +0800

    [feat] 隐藏微信首页指定用户的最后一条消息，初步适配

--- app/src/main/java/com/lu/wxmask/ClazzN.kt
+++ app/src/main/java/com/lu/wxmask/ClazzN.kt
@@ -4,5 +4,9 @@ interface ClazzN {
     companion object {
         const val BaseChattingUIFragment = "com.tencent.mm.ui.chatting.BaseChattingUIFragment"
         const val LauncherUI = "com.tencent.mm.ui.LauncherUI"
+        const val MainUI = "com.tencent.mm.ui.MainUI"
+        const val ConversationListView = "com.tencent.mm.ui.conversation.ConversationListView"
+        const val BaseContact = "com.tencent.mm.autogen.table.BaseContact"
+        const val BaseConversation = "com.tencent.mm.autogen.table.BaseConversation"
     }
 }
---
+++ app/src/main/java/com/lu/wxmask/util/AppVersionUtil.kt
@@ -0,0 +1,39 @@
+package com.lu.wxmask.util
+
+import com.lu.magic.util.AppUtil
+import com.lu.magic.util.log.LogUtil
+
+class AppVersionUtil {
+    companion object {
+        private var versionCode = -1
+        private var versionName = ""
+        fun getVersionName(): String? {
+            if (versionName.isBlank()) {
+                versionName = try {
+                    val manager = AppUtil.getContext().packageManager
+                    val info = manager.getPackageInfo(AppUtil.getContext().packageName, 0)
+                    return info.versionName
+                } catch (e: Exception) {
+                    LogUtil.e(e)
+                    ""
+                }
+            }
+            return versionName
+        }
+
+        fun getVersionCode(): Int {
+            if (versionCode == -1) {
+                versionCode = try {
+                    val manager = AppUtil.getContext().packageManager
+                    val info = manager.getPackageInfo(AppUtil.getContext().packageName, 0)
+                    info.versionCode
+                } catch (e: Exception) {
+                    LogUtil.e(e)
+                    -1
+                }
+            }
+            return versionCode
+        }
+
+    }
+}
