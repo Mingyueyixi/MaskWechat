b2d4c83281e1b2011322e1d40488956c16387510
Mingyueyixi <1379598589@qq.com>
2023-02-04 03:36:53 +0800

    [mod] 远程菜单cdn改为主分支

--- app/src/main/java/com/lu/wxmask/ui/JsonMenuManager.kt
+++ app/src/main/java/com/lu/wxmask/ui/JsonMenuManager.kt
@@ -112,7 +112,7 @@ class JsonMenuManager {
 
         fun updateMenuListFromRemoteIfNeed(ctx: Context) {
             // 2 hour
-            if (System.currentTimeMillis() - lastUpdateSuccessMills > 1000 * 60 * 60 * 2) {
+            if (!isRemoteUpdating && System.currentTimeMillis() - lastUpdateSuccessMills > 1000 * 60 * 60 * 2) {
                 updateMenuListFromRemote(ctx)
             }
         }
@@ -133,7 +133,7 @@ class JsonMenuManager {
                     //val githubRawPath = "https://github.com/$releatePath"
                     //@main分支 或者@v1.6， commit id之类的，直接在写/main有时候不行
                     //不指定版本，则取最后一个https://www.jsdelivr.com/?docs=gh
-                    val cdnRawPath = "https://cdn.jsdelivr.net/gh/Mingyueyixi/MaskWechat/$releatePath"
+                    val cdnRawPath = "https://cdn.jsdelivr.net/gh/Mingyueyixi/MaskWechat@main/$releatePath"
                     LogUtil.i("request $cdnRawPath")
                     HttpConnectUtil.get(cdnRawPath, HttpConnectUtil.noCacheHttpHeader) { cdnRes ->
                         if (cdnRes.error == null && cdnRes.code == 200 && cdnRes.body.isNotEmpty()) {
