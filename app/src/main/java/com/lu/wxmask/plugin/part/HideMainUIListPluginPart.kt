ce637a81b55d7c3f92b550b6783ff8185dea8421
Mingyueyixi <1379598589@qq.com>
2023-04-14 16:56:08 +0800

    [mod] 修复8.0.34主页上一次的消息条目未隐藏问题，未适配的版本如果走到此处，则采取暴力隐藏，昵称、时间、消息置空的方式

--- app/src/main/java/com/lu/wxmask/plugin/part/HideMainUIListPluginPart.kt
+++ app/src/main/java/com/lu/wxmask/plugin/part/HideMainUIListPluginPart.kt
@@ -1,7 +1,6 @@
 package com.lu.wxmask.plugin.part
 
 import android.content.Context
-import android.util.Log
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ListAdapter
@@ -10,15 +9,12 @@ import com.lu.lposed.api2.XC_MethodHook2
 import com.lu.lposed.api2.XposedHelpers2
 import com.lu.lposed.plugin.IPlugin
 import com.lu.magic.util.AppUtil
-import com.lu.magic.util.GsonUtil
 import com.lu.magic.util.log.LogUtil
 import com.lu.magic.util.view.ChildDeepCheck
-import com.lu.magic.util.view.SelfDeepCheck
 import com.lu.wxmask.ClazzN
 import com.lu.wxmask.Constrant
 import com.lu.wxmask.plugin.WXMaskPlugin
 import com.lu.wxmask.util.AppVersionUtil
-import com.lu.wxmask.util.dev.DebugUtil
 import de.robv.android.xposed.callbacks.XC_LoadPackage
 import java.lang.reflect.Method
 
@@ -35,7 +31,7 @@ class HideMainUIListPluginPart : IPlugin {
     //隐藏指定用户的主页的消息
     private fun handleMainUIChattingListView(context: Context, lpparam: XC_LoadPackage.LoadPackageParam) {
         val adapterName = when (AppVersionUtil.getVersionCode()) {
-            Constrant.WX_CODE_8_0_32, Constrant.WX_CODE_8_0_33, Constrant.WX_CODE_8_0_34-> "com.tencent.mm.ui.conversation.p"
+            Constrant.WX_CODE_8_0_32, Constrant.WX_CODE_8_0_33, Constrant.WX_CODE_8_0_34 -> "com.tencent.mm.ui.conversation.p"
             Constrant.WX_CODE_8_0_22 -> "com.tencent.mm.ui.conversation.k"
             else -> null
         }
@@ -91,16 +87,22 @@ class HideMainUIListPluginPart : IPlugin {
 
 //                    LogUtil.d("after getView", adapter.javaClass, GsonUtil.toJson(itemData))
                     if (baseConversationClazz?.isAssignableFrom(itemData.javaClass) != true
-                        && !itemData::class.java.name.startsWith("com.tencent.mm.storage")){
+                        && !itemData::class.java.name.startsWith("com.tencent.mm.storage")
+                    ) {
                         //不是所需类型
                         //LogUtil.d(chatUser, GsonUtil.toJson(itemData))
-                        LogUtil.w(AppVersionUtil.getVersionCode(), "类型检查错误，尝试继续", itemData::class.java, itemData::class.java.classes)
+                        LogUtil.w(
+                            AppVersionUtil.getVersionCode(),
+                            "类型检查错误，尝试继续",
+                            itemData::class.java,
+                            itemData::class.java.classes
+                        )
                     }
                     val chatUser: String = XposedHelpers2.getObjectField(itemData, "field_username") ?: return
                     val itemView: View = param.args[1] as? View ?: return
                     if (WXMaskPlugin.containChatUser(chatUser)) {
                         hideUnReadTipView(itemView, param)
-                        hideLastMsgView(itemView, param)
+                        hideMsgViewItemText(itemView, param)
                     }
                 }
 
@@ -125,22 +127,36 @@ class HideMainUIListPluginPart : IPlugin {
 
                 }
 
-                //隐藏最后一条消息
-                private fun hideLastMsgView(itemView: View, param: MethodHookParam) {
+                //隐藏最后一条消息等
+                private fun hideMsgViewItemText(itemView: View, param: MethodHookParam) {
                     val resource = AppUtil.getContext().resources
                     val msgTvIdName = when (AppVersionUtil.getVersionCode()) {
-                        in Constrant.WX_CODE_8_0_32 .. Constrant.WX_CODE_8_0_34-> "fhs"
+                        in Constrant.WX_CODE_8_0_32..Constrant.WX_CODE_8_0_33 -> "fhs"
                         else -> "last_msg_tv"
                     }
                     val lastMsgViewId = resource.getIdentifier(msgTvIdName, "id", AppUtil.getContext().packageName)
                     LogUtil.d("mask last msg textView", lastMsgViewId)
-                    val msgTv: View? = itemView.findViewById(lastMsgViewId)
-
-                    try {
-                        XposedHelpers2.callMethod<Any?>(msgTv, "setText", "")
-                    } catch (e: Throwable) {
-                        LogUtil.w("error", msgTv)
+                    if (lastMsgViewId != 0 && lastMsgViewId != View.NO_ID) {
+                        try {
+                            val msgTv: View? = itemView.findViewById(lastMsgViewId)
+                            XposedHelpers2.callMethod<Any?>(msgTv, "setText", "")
+                        } catch (e: Throwable) {
+                            LogUtil.w("error", e)
+                        }
+                    } else {
+                        //
+                        LogUtil.w("主页last消息id版本不适配，开启暴力隐藏", AppVersionUtil.getVersionCode())
+                        val ClazzNoMeasuredTextView = ClazzN.from("com.tencent.mm.ui.base.NoMeasuredTextView")
+                        ChildDeepCheck().each(itemView) { child ->
+                            try {
+                                if (ClazzNoMeasuredTextView?.isAssignableFrom(child::class.java) == true) {
+                                    XposedHelpers2.callMethod<String?>(child, "setText", "")
+                                }
+                            } catch (e: Throwable) {
+                            }
+                        }
                     }
+
                 }
 
             })
